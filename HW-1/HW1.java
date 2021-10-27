432480a95983d14b5514bdea18839b1924a1bbf9
Ivashka222 <92866479+Ivashka222@users.noreply.github.com>
2021-10-27 21:44:24 +0300

    Update HW1

--- HW-1/HW1.java
+++ HW-1/HW1.java
@@ -1,3 +1,15 @@
+import java.util.Scanner;
+
 public class HW1 {
-    
+    public static void ivan(String... args){
+
+        Scanner height = new Scanner(System.in);
+        System.out.print("Введіть перше значення: ");
+        int one = height.nextInt();
+        Scanner count = new Scanner(System.in);
+        System.out.print("Введіть друге значення: ");
+        int two = count.nextInt();
+        System.out.printf("The height building %d m and %d floors", one, two);
+        height.close(); count.close();
+    }
 }
